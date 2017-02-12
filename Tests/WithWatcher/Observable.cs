ddb0e0e8c8249630663c2e8ead59d42a9d5c41c4
marhoily <marhoily@homtail.com>
2017-02-12 14:50:53 +0300

    Make watcher keep dispose handler

--- Tests/WithWatcher/Observable.cs
+++ Tests/WithWatcher/Observable.cs
@@ -9,38 +9,51 @@ namespace Tests.WithWatcher
     internal sealed class Watcher
     {
         private readonly string _propertyName;
+        private readonly Action _changeHandler;
+        private Action _untrack;
 
-        public Watcher(string propertyName)
+        public Watcher(string propertyName, Action changeHandler)
         {
             _propertyName = propertyName;
+            _changeHandler = changeHandler;
         }
 
         public object Look(object premise) => premise
-                .GetType()
-                .GetProperty(_propertyName)
-                .GetValue(premise);
+            .GetType()
+            .GetProperty(_propertyName)
+            .GetValue(premise);
 
-        public Action Track(INotifyPropertyChanged premise, Action onChange)
-            => premise.Track(_propertyName, onChange);
+        public void Track(INotifyPropertyChanged premise)
+        {
+            _changeHandler();
+            _untrack = premise.Track(_propertyName, _changeHandler);
+        }
+
+        public void Untrack()
+        {
+            _untrack?.Invoke();
+            _untrack = null;
+        }
     }
+
     internal sealed class Observable : IDisposable
     {
         private readonly Observable _premiseSource;
         private INotifyPropertyChanged _premise;
         private readonly Watcher _watcher;
         public object Fact { get; private set; }
-        private Action _disposePremiseTracking;
         public event Action FactChanged;
 
-        public Observable(INotifyPropertyChanged premise, Watcher watcher)
+        public Observable(INotifyPropertyChanged premise, string propertyName)
         {
-            _watcher = watcher;
+            _watcher = new Watcher(propertyName, UpdateValue);
             ChangePremise(premise);
         }
-        public Observable(Observable premiseSource, Watcher watcher)
+
+        public Observable(Observable premiseSource, string propertyName)
         {
             _premiseSource = premiseSource;
-            _watcher = watcher;
+            _watcher = new Watcher(propertyName, UpdateValue);
             _premiseSource.FactChanged += OnPremiseChanged;
             OnPremiseChanged();
         }
@@ -51,8 +64,7 @@ namespace Tests.WithWatcher
                 return;
             if (_premise != null)
             {
-                _disposePremiseTracking?.Invoke();
-                _disposePremiseTracking = null;
+                _watcher.Untrack();
             }
             else
             {
@@ -61,19 +73,19 @@ namespace Tests.WithWatcher
             _premise = premise;
             if (_premise != null)
             {
-                UpdateValue();
-                _disposePremiseTracking = _watcher.Track(_premise, UpdateValue);
+                _watcher.Track(_premise);
             }
             else
             {
                 1.ToString();
             }
         }
+
         private void OnPremiseChanged()
         {
             ChangePremise(_premiseSource.Fact as INotifyPropertyChanged);
         }
-        
+
         private void UpdateValue()
         {
             var value = _watcher.Look(_premise);
@@ -85,7 +97,7 @@ namespace Tests.WithWatcher
 
         public void Dispose()
         {
-            _disposePremiseTracking?.Invoke();
+            _watcher.Untrack();
             if (_premiseSource != null)
             {
                 _premiseSource.FactChanged -= OnPremiseChanged;
@@ -96,20 +108,21 @@ namespace Tests.WithWatcher
             }
         }
     }
+
     internal static class Ext
     {
         public static Observable Observe(this INotifyPropertyChanged source, string propertyName, Action handler = null)
         {
-            var observable = new Observable(source, new Watcher(propertyName));
+            var observable = new Observable(source, propertyName);
             if (handler != null) observable.FactChanged += handler;
             return observable;
         }
+
         public static Observable Observe(this Observable source, string propertyName, Action handler = null)
         {
-            var observable = new Observable(source, new Watcher(propertyName));
+            var observable = new Observable(source, propertyName);
             if (handler != null) observable.FactChanged += handler;
             return observable;
         }
     }
-
 }
