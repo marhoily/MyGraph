f2afdba1e7d74e7a32ee4fde61a4bbaa34a23e0f
marhoily <marhoily@homtail.com>
2017-03-06 00:15:41 +0300

    Throw exceptions on binding errors

--- Wpf/App.xaml.cs
+++ Wpf/App.xaml.cs
@@ -1,10 +1,41 @@
-﻿namespace MyGraph
+﻿using System;
+using System.Diagnostics;
+
+namespace MyGraph
 {
     public partial class App
     {
         public App()
         {
             InitializeComponent();
+            PresentationTraceSources.DataBindingSource.Listeners
+                .Add(new BindingErrorListener());
+
+        }
+    }
+    public class BindingErrorListener : TraceListener
+    {
+        public override void Write(string message)
+        {
+            try
+            {
+                throw new Exception(message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        public override void WriteLine(string message)
+        {
+            try
+            {
+                throw new Exception(message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
