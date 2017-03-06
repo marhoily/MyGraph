bca5ddcd280a3c32cb0ce9bfafef179d3840874f
marhoily <marhoily@homtail.com>
2017-03-06 11:32:23 +0300

    Create nodes

--- Wpf/RootViewModel.cs
+++ Wpf/RootViewModel.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows;
 using Caliburn.Micro;
 
@@ -8,12 +10,35 @@ namespace MyGraph
     {
         public Graph Graph { get; }
 
+        public void Create300()
+        {
+            var rnd = new Random();
+            for (var i = 0; i < 300; i++)
+                Graph.Vertices.Add(new Vertex(new Point(
+                    rnd.NextDouble() * 500,
+                    rnd.NextDouble() * 500)));
+
+            var edges = Graph.Vertices.Zip(Graph.Vertices.Skip(1), (a, b) => new Edge(a, b));
+            foreach (var edge in edges)
+                Graph.Edges.Add(edge);
+        }
+        public void Create1000()
+        {
+            var rnd = new Random();
+            for (var i = 0; i < 1000; i++)
+                Graph.Vertices.Add(new Vertex(new Point(
+                    rnd.NextDouble() * 500,
+                    rnd.NextDouble() * 500)));
+
+            var edges = Graph.Vertices.Zip(Graph.Vertices.Skip(1), (a, b) => new Edge(a, b));
+            foreach (var edge in edges)
+                Graph.Edges.Add(edge);
+        }
         public RootViewModel()
         {
-            var a = new Vertex(new Point(100, 100));
-            var b = new Vertex(new Point(300, 200));
-            Graph = new Graph(new ObservableCollection<IVertex>{a,b},
-                new ObservableCollection<IEdge> {new Edge(a, b)});
+            Graph = new Graph(
+                new ObservableCollection<IVertex>(), 
+                new ObservableCollection<IEdge>());
         }
     }
 }
