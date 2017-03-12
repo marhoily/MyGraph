25b35c6c9a2bd577d0e7d139b70d251d1079b603
marhoily <marhoily@homtail.com>
2017-03-12 14:59:58 +0300

    Fix zoom issue

--- Wpf/MapView/MapView.xaml.cs
+++ Wpf/MapView/MapView.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using GMap.NET;
+using GMap.NET.MapProviders;
 
 namespace MyGraph
 {
@@ -7,7 +12,19 @@ namespace MyGraph
         public MapView()
         {
             InitializeComponent();
-            _gMapControl.DragButton = MouseButton.Left;
+            _gMapControl.MapProvider = GMapProviders.OpenStreetMap;
+            _gMapControl.Position = new PointLatLng(53.856, 27.49);
+            _gMapControl.MinZoom = 1;
+            _gMapControl.MaxZoom = 20;
+            _gMapControl.Zoom = 7;
+        }
+
+        private async void MapView_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            _gMapControl.Zoom = 9;
+            Debug.WriteLine(_gMapControl.MapProvider.Projection?.ToString() ?? "<null>");
         }
     }
 }
